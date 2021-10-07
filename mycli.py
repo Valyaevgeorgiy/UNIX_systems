caa3411023b60db24d0e0baf1504ec3d0ccaa8ac
egor_Mint <valyaev2002@bk.ru>
2021-10-07 17:46:33 +0300

    update client-server python files

--- mycli.py
+++ mycli.py
@@ -4,13 +4,20 @@
 import socket
 
 sock = socket.socket()
+print("---- Начало соединения с сервером ----")
 sock.connect(('localhost', 9090))
 
 while True:
+    print("---- Подключение к серверу успешно пройдено ----")
+    print("---- Начало ожидания порции данных на сервере ----")
     word = input()
+    print("---- Отправка данных на сервер ----")
     sock.send(word.encode())
     data = sock.recv(1024).decode()
     if "EXIT" in data:
+        print("---- Разрыв соединения с сервером ----")
         sock.close()
         break
+    print("---- Приём данных от сервера ----")
+    print()
     print(data)
--- myserv.py
+++ myserv.py
@@ -2,20 +2,28 @@
  
 import socket
 import os
- 
+
+print("---- Запуск сервера ----")
 sock = socket.socket()
 sock.bind(('', 9090))
+print("---- Начало прослушивания порта ----")
 sock.listen(1)
 conn, addr = sock.accept()
- 
-print('connected:', addr)
+print("---- Подключение клиента ----")
+print()
+print("----", '\t connected:', addr, "\t----")
 
 while True: 
     while True:
+        print("---- Приём данных от клиента ----")
         data = conn.recv(1024).decode()
         if not data:
             break
+        print("---- Отправка порции данных ----")
         conn.send(data.upper().encode())
         if "exit".lower() in data:
+            print("---- Заканчивается сеанс подключения и начинается выход ----")
+            print()
+            print("---- Остановка сервера ----")
             conn.close()
             os._exit(0)
