091d15c06d2329991d6db46cb4c5a1a54cda569e
Egor_Mint <valyaev2002@bk.ru>
2021-10-07 18:09:17 +0300

    second update files

--- mycli.py
+++ mycli.py
@@ -3,21 +3,27 @@
 
 import socket
 
+host_num = input("Введите желаемый номер порта от 1025 до 65535 (по умолчанию стоит 9090): ")
+if len(host_num) == 0:
+    host_num = 9090
+else:
+    host_num = int(host_num)
+
 sock = socket.socket()
-print("---- Начало соединения с сервером ----")
-sock.connect(('localhost', 9090))
+print("[INFO] ---- Начало соединения с сервером ----")
+sock.connect(('localhost', host_num))
+print("[INFO] ---- Подключение к серверу успешно пройдено ----")
 
 while True:
-    print("---- Подключение к серверу успешно пройдено ----")
-    print("---- Начало ожидания порции данных на сервере ----")
+    print("[INFO] ---- Начало ожидания порции данных на сервере ----")
     word = input()
-    print("---- Отправка данных на сервер ----")
+    print("[INFO] ---- Отправка данных на сервер ----")
     sock.send(word.encode())
     data = sock.recv(1024).decode()
     if "EXIT" in data:
-        print("---- Разрыв соединения с сервером ----")
+        print("[INFO] ---- Разрыв соединения с сервером ----")
         sock.close()
         break
-    print("---- Приём данных от сервера ----")
+    print("[INFO] ---- Приём данных от сервера ----")
     print()
     print(data)
--- myserv.py
+++ myserv.py
@@ -3,27 +3,36 @@
 import socket
 import os
 
-print("---- Запуск сервера ----")
+print("[INFO] ---- Запуск сервера ----")
+
 sock = socket.socket()
-sock.bind(('', 9090))
-print("---- Начало прослушивания порта ----")
+sock.bind(('', host_num))
+
+print("[INFO] ---- Начало прослушивания порта ----")
+
 sock.listen(1)
-conn, addr = sock.accept()
-print("---- Подключение клиента ----")
+conn, adr = sock.accept()
+
+print("[INFO] ---- Подключение клиента ----")
 print()
-print("----", '\t connected:', addr, "\t----")
+print("[INFO] ----", '\t Установлено соединение с IP', adr, "\t----")
 
 while True: 
     while True:
-        print("---- Приём данных от клиента ----")
+        print("[INFO] ---- Приём данных от клиента ----")
+
         data = conn.recv(1024).decode()
+
         if not data:
             break
-        print("---- Отправка порции данных ----")
+
+        print()
+        print("[INFO] ---- Отправка порции данных ----")
         conn.send(data.upper().encode())
+
         if "exit".lower() in data:
-            print("---- Заканчивается сеанс подключения и начинается выход ----")
+            print("[INFO] ---- Заканчивается сеанс подключения и начинается выход ----")
             print()
-            print("---- Остановка сервера ----")
+            print("[INFO] ---- Остановка сервера ----")
             conn.close()
             os._exit(0)
